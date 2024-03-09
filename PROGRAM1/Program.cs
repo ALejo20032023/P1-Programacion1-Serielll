c214b297ac282facb100d138af3540eef0942cc3
ALejo20032023 <jmorales.ipedep@gmail.com>
2024-03-09 12:49:17 -0600

    Se agrego

---
+++ PROGRAM1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido inge wapo");
+            Console.WriteLine("Por favor ingresa tu nombre y apellido:");
+
+
+            string nombre = Console.ReadLine();
+            string apellido = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el dominio de correo electrónico (ej. gmail.com):");
+            string dominio = Console.ReadLine();
+
+            string correo = GenerarCorreoElectronico(nombre, apellido, dominio);
+            Console.WriteLine("El correo electrónico generado es: " + correo);
+
+            Console.ReadLine();
+        }
+
+        static string GenerarCorreoElectronico(string nombre, string apellido, string dominio)
+        {
+            string primeras3LetrasNombre = nombre.Substring(0, 3).ToLower();
+            string primeras3LetrasApellido = apellido.Substring(0, 3).ToLower();
+
+            return primeras3LetrasNombre + primeras3LetrasApellido + "@" + dominio;
+        }
+    }
+
+}
